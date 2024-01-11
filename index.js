c98af341f281eb0a31b827f9a882343ab173d395
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-11 15:27:22 +0530

    Updated animations

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let toggle = document.querySelector(".toggleContainer");
 let circle = document.querySelector("#circleWhite");
 let text = document.querySelector(".h1Tag");
 let x = document.querySelector("#svg1");
-let y = document.querySelector("#svg2")
+let y = document.querySelector("#svg2");
 
+let isDarkMode = true;
+let lightMode = true;
 document.body.style.backgroundColor = "white";
 
 toggle.addEventListener("click", () => {
@@ -13,16 +15,17 @@ toggle.addEventListener("click", () => {
     circle.style.left = "55%";
     circle.style.backgroundColor = "Orange";
     text.style.color = "orange";
-    x.style.transform = "rotate(360deg)";
-    x.style.transitionDuration = "0.5s"
-
+    x.style.transform = `rotate(${isDarkMode ? 360 : 0}deg)`;
+    isDarkMode = !isDarkMode;
+    x.style.transitionDuration = "0.5s";
   } else {
     document.body.style.backgroundColor = "white";
     text.style.color = "black";
     circle.style.left = "2%";
     circle.style.backgroundColor = "white";
     text.style.color = "black";
-    y.style.transform = "rotate(360deg)";
-    y.style.transitionDuration = ".5s"
+    y.style.transform = `rotate(${lightMode ? 360 : 0}deg)`;
+    lightMode = !lightMode;
+    y.style.transitionDuration = ".5s";
   }
 });
