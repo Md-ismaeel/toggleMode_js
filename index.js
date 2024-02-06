e137f14e21c0eeef52358c991ee67889be439238
Md Ismail <mdismaeelkhan345@gmail.com>
2024-02-06 17:10:26 +0530

    Fixed Issue

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-let toggle = document.querySelector(".toggleContainer");
+// let toggle = document.querySelector(".toggleContainer");
 let circle = document.querySelector("#circleWhite");
 let text = document.querySelector(".h1Tag");
-let x = document.querySelector("#svg1");
-let y = document.querySelector("#svg2");
+let svg1 = document.querySelector("#svg1");
+let svg2 = document.querySelector("#svg2");
 
 let isDarkMode = true;
 let lightMode = true;
 document.body.style.backgroundColor = "white";
 
-toggle.addEventListener("click", () => {
+circle.addEventListener("click", () => {
   if (document.body.style.backgroundColor === "white") {
     document.body.style.backgroundColor = "black";
     text.style.color = "white";
     circle.style.left = "55%";
     circle.style.backgroundColor = "Orange";
     text.style.color = "orange";
-    x.style.transform = `rotate(${isDarkMode ? 360 : 0}deg)`;
+    svg1.style.transform = `rotate(${isDarkMode ? 360 : 0}deg)`;
     isDarkMode = !isDarkMode;
-    x.style.transitionDuration = "0.5s";
-    // text.style.transform = "translateX(-20px)";
+    svg1.style.transitionDuration = "0.5s";
     text.style.transform = "translateY(40px)";
     text.style.transitionDuration = "0.5s"
   } else {
@@ -27,9 +26,9 @@ toggle.addEventListener("click", () => {
     circle.style.left = "2%";
     circle.style.backgroundColor = "white";
     text.style.color = "black";
-    y.style.transform = `rotate(${lightMode ? 360 : 0}deg)`;
+    svg2.style.transform = `rotate(${lightMode ? 360 : 0}deg)`;
     lightMode = !lightMode;
-    y.style.transitionDuration = ".5s";
+    svg2.style.transitionDuration = ".5s";
     text.style.transform = "translateY(70px)";
     text.style.transitionDuration = "0.5s";
   }
